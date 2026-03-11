c89d4b2450a5e5cd4b443ded7587c55117e52257
Soubhagya Kumar Mishra <soubhagyakumarmishra66@gmail.com>
2026-03-11 23:33:38 +0530

    Commit

---
+++ Day - 8/datatypes.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 12.34;
+let c = "Silu";
+let d = true;
+
+let e = {
+    name : "Silu",
+    age : 22,
+    isStudent : true
+
+}
+
+let f = function silu () {
+
+}
+
+let g;
+let h = null;
+let i = Symbol("id");
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+console.log(typeof(h));
+console.log(typeof(i));
