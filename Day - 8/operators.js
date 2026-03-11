8bde494b0d9326b97f4afc4c6b594085224d8515
Soubhagya Kumar Mishra <soubhagyakumarmishra66@gmail.com>
2026-03-11 23:43:19 +0530

    Commit

---
+++ Day - 8/operators.js
@@ -0,0 +1,40 @@
+let a = 30;
+let b = 10;
+
+//Arithmetic Operator
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+//Assignment Operator
+
+console.log(a = 10);
+console.log(a += 5);
+console.log(a -= 3);
+console.log(a *= 4);
+console.log(a /= 7);
+
+//Comparision Operators
+
+let c = "10";
+let d = 10
+
+console.log(c == d);
+console.log(c === d);
+console.log(c >= d);
+console.log(c != d);
+console.log(c <= d);
+console.log(c < d);
+console.log(c > d);
+
+//Logical Operator
+
+console.log(a & b);
+console.log(a | b);
+
+//Ternary Operator
+
+let target = a <= b ? true:false;
+console.log(target);
